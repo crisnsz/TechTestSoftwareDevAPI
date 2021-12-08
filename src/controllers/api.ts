aa9bd4aca93b7d4c1ba3148a70721fa586ca20fd
Cristian Sanchez <alexandersmz92@gmail.com>
2021-12-08 17:43:53 -0600

    Remove Redis

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
 import { Application, Request, Response } from "express";
-import { createClient } from "redis";
-
-const client = createClient();
 
 export const loadApiEndpoints = async (app: Application): Promise<void> => {
   //Open Redis Connection
-  await client.connect();
 
   app.get("/api", (req: Request, res: Response) => {
     return res.status(200).send("Succeed");
@@ -15,22 +11,13 @@ export const loadApiEndpoints = async (app: Application): Promise<void> => {
   //Get User Info for Torre with id
   app.get("/TorreUserInfo", async (req: Request, res: Response) => {
     try {
-      const value = await client.get("TorreUserInfo");
-      if (value) {
-        return res.status(200).send(JSON.parse(value));
-      }
-
       const axiosResponse = await axios.get(
         "https://bio.torre.co/api/bios/cristiansanchez9824"
       );
-
-      await client.set("TorreUserInfo", JSON.stringify(axiosResponse.data));
 
       return res.status(200).send(axiosResponse.data);
     } catch (err) {
       console.log(err);
     }
   });
-
-  client.disconnect();
 };
